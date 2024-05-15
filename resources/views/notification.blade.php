7b26fe9fd0308b494504d14b47d05b748b383c22
M-Arham Waheed <info.epgae.org@gmail.com>
2024-05-15 16:31:02 +0500

    C3

--- resources/views/notification.blade.php
+++ resources/views/notification.blade.php
@@ -9,24 +9,31 @@
     <div id="reloadDiv" class="shadow-dark mt-3  px-5 rounded-xl pt-8  bg-white">
         <div class="w-full pb-5">
 
-            <a href="#" class="flex px-4 py-3  hover:bg-gray-100 dark:hover:bg-gray-700">
+            <a href="#" class=" flex px-4 py-3  hover:bg-gray-100 dark:hover:bg-gray-700">
 
                 <div class="w-full ps-3 pb-3 border-b-2 border-primary flex justify-between">
-                    <div class="text-gray-500 text-sm mb-1.5 dark:text-gray-400">New message from <span
-                            class="font-semibold text-gray-900 dark:text-white">Jese Leos</span>: "Hey,
-                        what's up? All set for the presentation?"</div>
-                    <div class="text-xs text-blue-600 dark:text-blue-500">a few moments ago</div>
-                </div>
-            </a>
-            <a href="#" class="flex px-4 pt-3 hover:bg-gray-100 dark:hover:bg-gray-700">
 
-                <div class="w-full ps-3 pb-3 border-b-2 border-primary flex justify-between">
-                    <div class="text-gray-500 text-sm mb-1.5 dark:text-gray-400">New message from <span
-                            class="font-semibold text-gray-900 dark:text-white">Jese Leos</span>: "Hey,
-                        what's up? All set for the presentation?"</div>
-                    <div class="text-xs text-blue-600 dark:text-blue-500">a few moments ago</div>
+                    <div class="text-gray-500 text-sm mb-1.5 dark:text-gray-400 flex">
+                        <div
+                            class="bg-primary text-white rounded-md hover:bg-transparent hover:text-primary border-2 hover:border-primary duration-300  h-[30px] w-[30px] flex justify-center items-center">
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6" />
+                            </svg>
+                        </div>
+                        <div class="pl-5 w-full">
+                            <h1 class="text-[18px] font-bold">New Message Heading!</h1>
+                            <p class="pt-2 w-[80%]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium
+                                laboriosam accusamus voluptates dolore</p>
+                        </div>
+                    </div>
+
+                    <div class="text-xs text-blue-600 dark:text-blue-500 w-[30%] text-end">a few moments ago</div>
                 </div>
             </a>
+
 
         </div>
     </div>
