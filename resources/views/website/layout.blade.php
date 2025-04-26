c78b0bf4634aa8b7762d759c2c7a250b09799d3d
MuhammadNasir1 <nmian7080@gmail.com>
2025-04-26 11:42:10 +0500

    change favicon

--- resources/views/website/layout.blade.php
+++ resources/views/website/layout.blade.php
@@ -7,7 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') - Horeca-Kaya</title>
-    <link rel="shortcut icon" href="{{ asset('images/circle-logo.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('images/favicon(32X32).png') }}" type="image/x-icon">
+    {{-- <link rel="shortcut icon" href="{{ asset('images/circle-logo.png') }}" type="image/x-icon"> --}}
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
